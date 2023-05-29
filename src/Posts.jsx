46ed027e6c206203ce6b149352c4b45eef3782bd
Knight-4000 <yorksprinters@gmail.com>
2023-05-29 10:11:01 -0400

    Update node

---
+++ src/Posts.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+import { PostDetail } from "./PostDetail";
+import { useQuery } from "react-query";
+const maxPostPage = 10;
+
+async function fetchPosts() {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+  );
+  return response.json();
+}
+
+export function Posts() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  // replace with useQuery
+  const {data} = useQuery("posts", fetchPosts);
+
+  // If fetchPosts has not resolved data yet, early return a div
+  // But when fetchPosts resolves, now we can map the data.
+if (!data) return <div />;
+
+  return (
+    <>
+      <ul>
+        {data.map((post) => (
+          <li
+            key={post.id}
+            className="post-title"
+            onClick={() => setSelectedPost(post)}
+          >
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <div className="pages">
+        <button disabled onClick={() => {}}>
+          Previous page
+        </button>
+        <span>Page {currentPage + 1}</span>
+        <button disabled onClick={() => {}}>
+          Next page
+        </button>
+      </div>
+      <hr />
+      {selectedPost && <PostDetail post={selectedPost} />}
+    </>
+  );
+}
